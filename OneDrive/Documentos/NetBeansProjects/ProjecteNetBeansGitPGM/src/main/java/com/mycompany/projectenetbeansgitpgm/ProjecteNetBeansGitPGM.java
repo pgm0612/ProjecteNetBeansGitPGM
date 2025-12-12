e86f6681fb7ceff7786a18684154ef6cc35bf687
Pau <pagmar@jviladoms.cat>
2025-12-12 10:20:33 +0100

    Comentari del primer commit desde netbeans

---
+++ OneDrive/Documentos/NetBeansProjects/ProjecteNetBeansGitPGM/src/main/java/com/mycompany/projectenetbeansgitpgm/ProjecteNetBeansGitPGM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+//SEGONA MODIFICACIO
+package com.mycompany.projectenetbeansgitpgm;
+
+/**
+ *
+ * @author pauga
+ */
+public class ProjecteNetBeansGitPGM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
